1f0da214d4e97731cf98520988fc901f5eeccd70
Thiago Ferreira Rodrigues <thiago.ferreirarod22@gmail.com>
2023-05-19 20:01:05 -0300

    feat: header styles on scroll

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
 const header = document.querySelector("header");
 const banner = document.querySelector(".about");
+const headerHeight = header.clientHeight;
 const bannerHeight = banner.clientHeight;
+const bannerYposition = banner.getBoundingClientRect();
+
+function checkHeaderPosition(){
+    if(window.scrollY > bannerHeight) {
+        header.classList.remove("in-banner");
+    } else {
+        header.classList.add("in-banner");
+    }
+}
+
+window.addEventListener('scroll', () => {
+    checkHeaderPosition();
+})
